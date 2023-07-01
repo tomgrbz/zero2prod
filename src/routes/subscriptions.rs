723bc83151fafc52dcbab38249fb16212afd51df
tomgrbz <tomgrbz@gmail.com>
2023-06-30 23:31:38 -0400

    CALL sqlx prepare to fix queries failing build for sqlx-offline in digital ocean and local builds of Dockerfiel

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -49,11 +49,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')"#,
+        VALUES ($1, $2, $3, $4, $5)"#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
-        Utc::now()
+        Utc::now(),
+        "confirmed",
     )
     .execute(pool)
     .await
